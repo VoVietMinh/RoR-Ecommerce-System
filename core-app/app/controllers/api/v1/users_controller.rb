362b068bb039eb7457fcc5cdeb5845e8616842f0
VoVietMinh <vietminh2222@gmail.com>
2021-08-07 11:03:09 +0700

    update user controller

--- core-app/app/controllers/api/v1/users_controller.rb
+++ core-app/app/controllers/api/v1/users_controller.rb
@@ -1,34 +1,35 @@
 class Api::V1::UsersController < Api::ApiBaseController
+  before_action :set_user, only: [:show, :edit, :destroy]
 
   def index
-    users = User.all()
-    render json: {
-      success: true,
-      data: users
-    }
-  end
-
-  def new
-    
+    @users = User.all()
+    json_response(@users)
   end
 
   def create
+    @user = User.create!(user_params)
+    json_response(@users, :create)
   end
 
   def show
-    user = User.find params[:id]
-    render json: {
-      success: true,
-      data: user
-    }
-  end
-
-  def edit
+    json_response(@user)
   end
 
   def update
+    @user = User.update(user_params)
+    json_response(@user)
   end
 
   def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  def user_params
+    params.permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
